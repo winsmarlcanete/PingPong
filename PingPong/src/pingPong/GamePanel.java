f03fe7f2e0b35fa1a7c9acb035164bdf9061a1e0
Winsmarl <110825438+winsmarlcanete@users.noreply.github.com>
2023-10-31 22:57:16 +0800

    Day 3 Commit
    must fix the ball speed, too fast

--- PingPong/src/pingPong/GamePanel.java
+++ PingPong/src/pingPong/GamePanel.java
@@ -37,10 +37,12 @@ public class GamePanel extends JPanel implements Runnable{
 	}
 	
 	public void newBall() {
-		
+		//random = new Random();
+		ball = new Ball((GAME_WIDTH/2)-(BALL_DIAMETER/2),(GAME_HEIGHT/2)-(BALL_DIAMETER/2),BALL_DIAMETER,BALL_DIAMETER);
 	}
 	public void newPaddles() {
-		
+		paddle1 = new Paddle(0,(GAME_HEIGHT/2)-(PADDLE_HEIGHT/2),PADDLE_WIDTH, PADDLE_HEIGHT,1);
+		paddle2 = new Paddle(GAME_WIDTH-PADDLE_WIDTH,(GAME_HEIGHT/2)-(PADDLE_HEIGHT/2),PADDLE_WIDTH, PADDLE_HEIGHT,2);
 	}
 	public void paint(Graphics g) {
 		image = createImage(getWidth(), getHeight());
@@ -49,13 +51,32 @@ public class GamePanel extends JPanel implements Runnable{
 		g.drawImage(image, 0, 0, this);
 	}
 	public void draw(Graphics g) {
-		
+		paddle1.draw(g);
+		paddle2.draw(g);
+		ball.draw(g);
 	}
 	public void move() {
-		
+		paddle1.move();
+		paddle2.move();
+		ball.move();
 	}
 	public void checkCollision() {
 		
+		if(ball.y <= 0) {
+			ball.setYDirection(-ball.yVelocity);
+		}
+		if(ball.y >= GAME_HEIGHT - BALL_DIAMETER) {
+			ball.setYDirection(-ball.yVelocity);
+		}
+		
+		if(paddle1.y<=0) 
+			paddle1.y=0;
+		if(paddle1.y >= (GAME_HEIGHT - PADDLE_HEIGHT))
+			paddle1.y = GAME_HEIGHT - PADDLE_HEIGHT;
+		if(paddle2.y<=0) 
+			paddle2.y=0;
+		if(paddle2.y >= (GAME_HEIGHT - PADDLE_HEIGHT))
+			paddle2.y = GAME_HEIGHT - PADDLE_HEIGHT;
 	}
 	public void run() {
 		long lastTime = System.nanoTime();
@@ -72,16 +93,17 @@ public class GamePanel extends JPanel implements Runnable{
 				checkCollision();
 				repaint();
 				delta--;
-				System.out.println("Test");
 			}
 		}
 	}
 	public class AL extends KeyAdapter{
 		public void keyPressed(KeyEvent e) {
-			
+			paddle1.keyPressed(e);
+			paddle2.keyPressed(e);
 		}
 		public void keyReleased(KeyEvent e) {
-			
+			paddle1.keyReleased(e);
+			paddle2.keyReleased(e);
 		}
 	}
 }
